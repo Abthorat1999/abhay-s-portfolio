e07631f0b559a88e20ca7652d8f4ad0f8e89c3d2
Abthorat1999 <111336226+Abthorat1999@users.noreply.github.com>
2024-05-17 09:15:31 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 // import axios from "axios";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as  Route, Routes, HashRouter } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <div>
           <Navbar />
           <Routes>
@@ -16,7 +16,7 @@ function App() {
             <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
